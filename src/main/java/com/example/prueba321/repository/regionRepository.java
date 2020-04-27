6aa6c75f87a5c28ec4a2f2bf1864c274f435e5a1
Josue Romero <jcromero@pucp.edu.pe>
2020-04-27 13:19:29 -0500

    commit region repository

---
+++ src/main/java/com/example/prueba321/repository/regionRepository.java
@@ -0,0 +1,11 @@
+package com.example.prueba321.repository;
+
+import com.example.prueba321.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface regionRepository extends JpaRepository<Region,Integer> {
+
+
+}
