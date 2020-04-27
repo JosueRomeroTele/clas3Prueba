55697e78739d9134e83cdb38b60d6bb171e391e1
Josue Romero <jcromero@pucp.edu.pe>
2020-04-27 12:15:00 -0500

    primer commit

---
+++ src/main/java/com/example/prueba321/Prueba321Application.java
@@ -0,0 +1,13 @@
+package com.example.prueba321;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba321Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prueba321Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prueba321/Prueba321ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba321;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba321ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
